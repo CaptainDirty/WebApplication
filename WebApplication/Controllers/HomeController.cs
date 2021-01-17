ac18552befd03114c48af8bccd8cada28be03fc0
Bolotov.Alexander@urfu.me <Bolotov.Alexander@urfu.me>
2021-01-17 19:29:46 +0500

    Добавил график приходной части теплового баланса

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -34,6 +34,8 @@ namespace WebApplication.Controllers
             co = fc.FinalResult(ci, mc);
             ViewBag.ClassOutputModels = co;
 
+            var IncomingPart = new List<double>() { Math.Round(co.Q_x, 2), Math.Round(co.Q_fiztopl, 2), Math.Round(co.Q_fizvozd, 2) };//Выводим график
+            ViewBag.TheIncomingPart = Newtonsoft.Json.JsonConvert.SerializeObject(IncomingPart);
             var ExpenditurePart = new List<double>() { Math.Round(co.Q1, 2), Math.Round(co.Q2, 2), Math.Round(co.Q3, 2), Math.Round(co.Q5_top, 2), Math.Round(co.Q5_rp, 2) };//Выводим график
             ViewBag.TheExpenditurePart = Newtonsoft.Json.JsonConvert.SerializeObject(ExpenditurePart);
             return View();
